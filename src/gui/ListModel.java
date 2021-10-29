a000c58fc65c7aef2bf21d5405dbab0b55a75879
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-10-29 14:23:09 +0200

    Removed solution

--- src/gui/ListModel.java
+++ src/gui/ListModel.java
@@ -1,39 +1,4 @@
 package gui;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 public class ListModel {
-    private ObservableList<PersonModel> people = FXCollections.observableArrayList();
-    private ObjectProperty<PersonModel> selectedPerson = new SimpleObjectProperty<>();
-    private StringProperty newPersonFirstname = new SimpleStringProperty();
-    private StringProperty newPersonLastname = new SimpleStringProperty();
-
-    public void add() {
-        people.add(new PersonModel(getNewPersonFirstname().get(), getNewPersonLastname().get()));
-    }
-
-    public void remove() {
-        people.remove(selectedPerson.get());
-    }
-
-    public ObservableList<PersonModel> getPeople() {
-        return people;
-    }
-
-    public StringProperty getNewPersonFirstname() {
-        return newPersonFirstname;
-    }
-
-    public StringProperty getNewPersonLastname() {
-        return newPersonLastname;
-    }
-
-    public ObjectProperty<PersonModel> getSelectedPerson() {
-        return selectedPerson;
-    }
 }
--- src/gui/PersonModel.java
+++ src/gui/PersonModel.java
@@ -4,20 +4,4 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class PersonModel {
-
-    private StringProperty firstname = new SimpleStringProperty();
-    private StringProperty lastname = new SimpleStringProperty();
-
-    public PersonModel(String firstname, String lastname) {
-        this.getFirstname().set(firstname);
-        this.getLastname().set(lastname);
-    }
-
-    public StringProperty getFirstname() {
-        return firstname;
-    }
-
-    public StringProperty getLastname() {
-        return lastname;
-    }
 }
